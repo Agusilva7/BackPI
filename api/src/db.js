0e87ecc28b08b9437c021b7e3c878c66bd15808b
Agusilva7 <niupi7374@gmail.com>
2023-09-29 12:20:31 -0300

    deploy db externa

--- api/src/db.js
+++ api/src/db.js
@@ -17,6 +17,7 @@ const sequelize = new Sequelize(DB_DEPLOY, {
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
